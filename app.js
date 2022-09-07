bd52bcf923fe27e43ced36a84bdfae9201fbbb50
Dmitriy Kolyada <Dmitriy.Kolyada.IT@gmail.com>
2022-09-07 16:44:04 -0400

    registration-user

--- app.js
+++ app.js
@@ -6,18 +6,19 @@ const path = require('path');
 const checkDbConnection = require('./db/config/checkDbConnection');
 
 const homeRoute = require('./src/routes/home.route');
+const regisnrationRoute = require('./src/routes/registration.route');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './public/')));
 
 app.use('/', homeRoute);
+app.use('/registration', regisnrationRoute);
 
 
+const { DEV_PORT } = process.env;
 
-const { PORT } = process.env;
-
-app.listen(PORT, () => {
-  console.log(`server started PORT: ${PORT}`);
+app.listen(DEV_PORT, () => {
+  console.log(`server started PORT: ${DEV_PORT}`);
   checkDbConnection();
 });
--- db/migrations/20220907140018-create-user.js
+++ db/migrations/20220907140018-create-user.js
@@ -1,4 +1,3 @@
-'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
@@ -6,34 +5,35 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       firstNameHR: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       lastNameHR: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true,
       },
       isAdmin: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Users');
-  }
+  },
 };
